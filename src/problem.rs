a5567d7ed4a2b2ba74866f9110a82b7b09bbbe63
Samuel Litchfield <samuel.litchfield@gmail.com>
2024-01-16 09:55:50 -0500

    Completed Day 2.

--- src/problem.rs
+++ src/problem.rs
@@ -6,7 +6,20 @@ pub struct Problem {
     pub log: String,
     pub part_1_answer: Option<i32>,
     pub part_2_answer: Option<i32>,
+    games: Vec<Game>,
+}
 
+#[derive(Default)]
+pub struct Game {
+    index: u32,
+    sets: Vec<CubeSet>,
+}
+
+#[derive(Default, Debug)]
+pub struct CubeSet {
+    red: u32,
+    green: u32,
+    blue: u32,
 }
 
 impl Problem {
@@ -14,111 +27,144 @@ impl Problem {
         self.input_raw = new_input.clone();
     }
 
+    pub fn parse_set(set_desc: &str) -> CubeSet {
+
+        let mut retset: CubeSet = CubeSet::default();
+        let splits: Vec<&str> = set_desc.split(", ").collect();
+        if splits.len() > 3 {
+            println!("Got more entries than RGB would allow!");
+        }
+        for item in splits {
+            let item_split: Vec<&str> = item.split(" ").collect();
+            let value = item_split.first().unwrap().parse::<u32>().unwrap();
+            let color = item_split.last().unwrap();
+            if color.ends_with("red") {
+                retset.red = value;
+                continue;
+            }
+            if color.ends_with("green") {
+                retset.green = value;
+                continue;
+            }
+            if color.ends_with("blue") {
+                retset.blue = value;
+                continue;
+            }
+        }
+        retset
+    }
+
+    pub fn parse_game(line: &str) -> Game {
+        // Split about Game X: 
+        println!("Parsing: {}", line);
+        let split = line.split(": ");
+        let clauses: Vec<&str> = split.collect();
+        let game_clause = clauses.first().unwrap();
+        let sets_clause = clauses.last().unwrap();
+
+        let split = game_clause.split(" ");
+        let clauses: Vec<&str> = split.collect();
+        let index = clauses.last().unwrap().parse::<u32>().unwrap();
+
+        let split = sets_clause.split("; ");
+        let sets: Vec<&str> = split.collect();
+        println!("Found index: {}", index);
+        println!("\tFound sets: {:?}", sets);
+
+        let parsed_sets: Vec<CubeSet> = sets.into_iter().map(Self::parse_set).collect();
+        println!("\tParsed sets as: {:?}", parsed_sets);
+        Game {
+            index,
+            sets: parsed_sets
+        }
+    }
+
+    pub fn is_game_possible(game: &Game) -> bool {
+        let mut retbool = true;
+        for set in &game.sets { 
+            if set.red > 12 {
+                println!("\tToo many reds; Invalid!");
+                retbool = false;
+            } 
+            if set.green > 13 {
+                println!("\tToo many greens; Invalid!");
+                retbool = false;
+            }
+            if set.blue > 14 {
+                println!("\tToo many blues; Invalid!");
+                retbool = false;
+            }
+        }
+        retbool
+    }
+
     pub fn process_as_part_1(&mut self) -> () {
         self.log.push_str("Processing as part 1\n");
         self.log.push_str("Input text: \n");
         self.log.push_str(format!("{}\n", self.input_raw).as_str());
 
-        let mut running_sum: i32 = 0;
-        for line in self.input_raw.lines() {
-            let mut left_digit: Option<u32> = None;
-            let mut right_digit: Option<u32> = None;
+        self.games.clear();
 
-            for char in line.chars() {
-                if char.is_digit(10) && matches!(left_digit, None) {
-                    left_digit = Some(char.to_digit(10)).expect("Could not parse digit"); 
-                }
-                if char.is_digit(10) {
-                    right_digit = Some(char.to_digit(10)).expect("Could not parse digit");
-                }
-            }
-            if matches!(left_digit, Some(u32)) && matches!(right_digit, Some(u32)) {
-                self.log.push_str(format!("Found {} and {}\n", left_digit.unwrap(), right_digit.unwrap()).as_str());
-                running_sum += (left_digit.unwrap() * 10 + right_digit.unwrap()) as i32;
+        for line in self.input_raw.lines() {
+            self.log.push_str(format!("Processing line: \"{}\"\n", line).as_str());
+            let line_game = Problem::parse_game(line);
+            self.games.push(line_game);
+        }
+
+        let mut id_sums = 0u32;
+
+        for game in &self.games {
+            println!("Processing Game {}: {:?}", game.index, game.sets);
+            if Self::is_game_possible(game) {
+                println!("\tGame is valid!");
+                id_sums += game.index;
             }
         }
-        self.part_1_answer = Some(running_sum);
+        self.part_1_answer = Some(id_sums as i32);
         ()
     }
-    
+
+    pub fn calculate_game_power(game: &Game) -> u32 {
+        let mut max_r = 0;
+        let mut max_g = 0;
+        let mut max_b = 0;
+        
+        for set in &game.sets { 
+            if set.red > max_r {
+                max_r = set.red;
+            } 
+            if set.green > max_g {
+                max_g = set.green;
+            }
+            if set.blue > max_b {
+                max_b = set.blue
+            }
+        }
+
+        max_r * max_g * max_b
+    }
+
     pub fn process_as_part_2(&mut self) -> () {
         self.log.push_str("Processing as part 2\n");
         self.log.push_str("Input text: \n");
         self.log.push_str(format!("{}\n", self.input_raw).as_str());
+        
+        self.games.clear();
 
-        let mut running_sum: i32 = 0;
         for line in self.input_raw.lines() {
-
-            let mut matches = vec![];
-            for i in 0..line.chars().count() {
-                if line[i..].starts_with(char::is_numeric) {
-                    matches.push(line.chars().nth(i).unwrap().to_digit(10).unwrap());
-                    continue
-                }
-                if line[i..].starts_with("one") {
-                    matches.push(1);
-                    continue
-                }
-                if line[i..].starts_with("two") {
-                    matches.push(2);
-                    continue
-                }
-                if line[i..].starts_with("three") {
-                    matches.push(3);
-                    continue
-                }
-                if line[i..].starts_with("four") {
-                    matches.push(4);
-                    continue
-                }
-                if line[i..].starts_with("five") {
-                    matches.push(5);
-                    continue
-                }
-                if line[i..].starts_with("six") {
-                    matches.push(6);
-                    continue
-                }
-                if line[i..].starts_with("seven") {
-                    matches.push(7);
-                    continue
-                }
-                if line[i..].starts_with("eight") {
-                    matches.push(8);
-                    continue
-                }
-                if line[i..].starts_with("nine") {
-                    matches.push(9);
-                    continue
-                }
-            }
-            self.log.push_str(format!("Line \"{}\" matched on: {:?}\n", line, matches).as_str());
-            let left_digit= matches.first();
-            let lvalue;
-            let right_digit = matches.last();
-            let rvalue;
-
-            match left_digit {
-                Some(n) => {lvalue = n }
-                None => {
-                    lvalue = &0;
-                    self.log.push_str(format!("WARNING: Could not find left value in \"{}\"", line).as_str());
-                }
-            }
-            match right_digit {
-                Some(n) => { rvalue = n }
-                None => {
-                    rvalue = &0;
-                    self.log.push_str(format!("WARNING: Could not find right value in \"{}\"", line).as_str());
-                }
-            }
-
-            running_sum += (lvalue * 10 + rvalue) as i32;
-            self.log.push_str(format!("\tExtracted: {}\n", lvalue * 10 + rvalue).as_str());
+            self.log.push_str(format!("Processing line: \"{}\"\n", line).as_str());
+            let line_game = Problem::parse_game(line);
+            self.games.push(line_game);
         }
-        self.log.push_str(format!("Final sum: {}\n", running_sum).as_str());
 
-        self.part_2_answer = Some(running_sum);
+        let mut running_sum = 0u32;
+
+        for game in &self.games {
+            println!("Processing Game {}: {:?}", game.index, game.sets);
+            running_sum += Self::calculate_game_power(game);
+        }
+
+        self.part_2_answer = Some(running_sum as i32);
         ()
     }
 }
